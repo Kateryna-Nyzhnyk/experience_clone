01ed2167ae8f88f6e02cfd3dc21b9ddd6caf9ef0
Kate Nyzhnyk <katya.lukash19@gmail.com>
2025-02-02 21:04:47 +0000

    test

---
+++ hw1.js
@@ -0,0 +1,4 @@
+number33 //'number' converted it to a string
+null + 3 output:3 //null is 0
+for 5 && qwerty output is qwerty //&& evaluates operands from left to right, returnin g immediately with the value of the first falsy operand it encounters
+for +'40' + +'2' + hillel output is 42hillel // When adding  a number to a string the interpreter converts number to a string and concatenates both together.
