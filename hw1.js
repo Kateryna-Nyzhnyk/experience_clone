e6c67655185fdb6fe813744ed8ef30649644d9fb
Kate Nyzhnyk <katya.lukash19@gmail.com>
2025-02-03 14:11:32 +0000

    5th commit

--- hw1.js
+++ hw1.js
@@ -12,3 +12,8 @@ for '10' + (5 === 6) output is false // 5 is not equal to 6 which is false, and 
 for null == '' output is false // because null is not equeal to quotes
 3 ** (9 / 3) output is 27 // first we devide 9/3, then we perform exponentiation 3*3*3
 git push'false' == git push'true' output is true // double marks exclamation are used to cast a value in a boolean, so 0 == 1 is true
+0 || '0' && 1 output is 1 //logical OR returns 0 when comparing 0 to 0, AND return 1 because one of the values is true 
+(+null == false) < 1 output is false // +null is 0, so 0 == false is true, and 1 > 1 is false 
+false && true || true output is true // false AND true returns true, true OR true returns true 
+false && (false || true) output is false // false OR true returns true, false AND true returns false because with && the result can be true is all operands are true 
+(+null == false) < 1 ** 5 output is false //(+null == false) is 0, 0 < 1 is false (0), 0 ** 5 is null (false) 
